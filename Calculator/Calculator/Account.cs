f756bc0cecd85c1bf7bc6dd07344b7c34a9b8c56
gavaskar7 <gavaskar.venkatesan@gmail.com>
2018-12-11 15:28:18 +0530

    Update Account.cs

--- Calculator/Calculator/Account.cs
+++ Calculator/Calculator/Account.cs
@@ -11,5 +11,7 @@ namespace Calculator
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string EMailID { get; set; }
+        
     }
 }
