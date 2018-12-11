9a1fd667070f15cffddbe65e97b125079ba2065c
Venkatesan, Gavaskar <gavaskar.venkatesan@hp.com>
2018-12-11 12:45:45 +0530

    Added Tests

--- Calculator/Calculator.Tests/CalculatorTests.cs
+++ Calculator/Calculator.Tests/CalculatorTests.cs
@@ -14,25 +14,37 @@ namespace Calculator.Tests
         [TestMethod()]
         public void AddTest()
         {
-            Assert.Fail();
+            Calculator calculator = new Calculator();
+            int expectedValue = 2;
+            int actualValue = calculator.Add(1, 1);
+            Assert.AreEqual(expectedValue, actualValue, "Add is not working");
         }
 
         [TestMethod()]
         public void SubtractTest()
         {
-            Assert.Fail();
+            Calculator calculator = new Calculator();
+            int expectedValue = 2;
+            int actualValue = calculator.Subtract(3, 1);
+            Assert.AreEqual(expectedValue, actualValue, "Subtract is not working");
         }
 
         [TestMethod()]
         public void MultiplyTest()
         {
-            Assert.Fail();
+            Calculator calculator = new Calculator();
+            int expectedValue = 20;
+            int actualValue = calculator.Multiply(10, 2);
+            Assert.AreEqual(expectedValue, actualValue, "Multiply is not working");
         }
 
         [TestMethod()]
         public void DivideTest()
         {
-            Assert.Fail();
+            Calculator calculator = new Calculator();
+            decimal expectedValue = 5;
+            decimal actualValue = calculator.Divide(10, 2);
+            Assert.AreEqual(expectedValue, actualValue, "Divide is not working");
         }
     }
 }
